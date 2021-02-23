2e7c85d75edbeb019f0f2b4405afe2da633cb8fd
Andrea Ligorio <andrea85kro@gmail.com>
2021-02-23 16:40:14 +0100

    Esercizio dado completato

--- js/main.js
+++ js/main.js
@@ -1,17 +1,48 @@
-//CALCOLO IL PRIMO NUMERO RANDOM
-var firstNumber = Math.floor(Math.random()*6) + 1;
-console.log(firstNumber);
 
-//CREO UNA VARIABILE DADO E MODIFICO LA CLASSE AGGIUNGENDO IL NUMERO RANDOM
-//IN MODO DA PRELEVARE LA FACCIA DEL DADO CORRISPONDENTE DAL CSS
-var dice = document.getElementById("dice");
-dice.className = dice.classList + firstNumber;
-console.log(dice);
+//STABILISCO AZIONI CON BUTTON START
+var start = document.getElementById("start");
+start.addEventListener("click",
+    function() {
 
-//RIPETO STESSA PROCEDURA PER IL SECONDO DADO
-var secondNumber = Math.floor(Math.random() * 6) + 1;
-console.log(secondNumber);
+        //CREO UNA VARIABILE CON IL NOME INSERITO IN INPUT
+        var playerName = document.getElementById("name_player").value;
+        document.getElementById("player").innerHTML = playerName;
+        
+        //CONTROLLO SE E' STATO INSERITO UN NOME USER IN INPUT
+        if (playerName == "") {
+            playerName = "User";
+            document.getElementById("player").innerHTML = playerName;
+        }
+        //CALCOLO IL PRIMO NUMERO RANDOM
+        var firstNumber = Math.floor(Math.random()*6) + 1;
+        
+        //CREO UNA VARIABILE DADO E MODIFICO LA CLASSE AGGIUNGENDO IL NUMERO RANDOM
+        //IN MODO DA PRELEVARE LA FACCIA DEL DADO CORRISPONDENTE DAL CSS
+        var dice = document.getElementById("dice");
+        dice.className = dice.classList + firstNumber;
+        
+        //RIPETO STESSA PROCEDURA PER IL SECONDO DADO
+        var secondNumber = Math.floor(Math.random() * 6) + 1;
+        
+        var diceTwo = document.getElementById("dice_two");
+        diceTwo.className = diceTwo.classList + secondNumber;
 
-var diceTwo = document.getElementById("dice_two");
-diceTwo.className = diceTwo.classList + secondNumber;
-console.log(diceTwo);
+        //CONTROLLO QUALE DEI 2 DADI VINCE
+        if (firstNumber > secondNumber) {
+            document.getElementById("player_win").innerHTML = playerName + " ha vinto";
+        } else if (firstNumber < secondNumber) {
+            document.getElementById("cpu_win").innerHTML = "La CPU ha vinto";
+        } else {
+            document.getElementById("player_win").innerHTML = "pareggio";
+            document.getElementById("cpu_win").innerHTML = "pareggio";
+        }
+    }
+);
+
+//STABILISCO AL BUTTON RESET IL REFRESH DELLA PAGINA
+var reset = document.getElementById("reset");
+reset.addEventListener("click", 
+    function() {
+        location.reload();
+    }
+);
